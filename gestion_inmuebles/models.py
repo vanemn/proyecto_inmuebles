fac25726dadecc30f3bc28f335aa4870ee0f0b71
Vane <vane.moralesn@gmail.com>
2024-11-18 20:31:56 -0300

    en archivo models.py de nuestra carpeta app se crea un modelo basico y se verifican las migraciones en PSQL

--- gestion_inmuebles/models.py
+++ gestion_inmuebles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Inmueble(models.Model):
+
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    m2_construidos = models.FloatField()
+    m2_totales = models.FloatField()
+    estacionamientos = models.IntegerField()
+    habitaciones = models.IntegerField()
+    banos = models.IntegerField()
+    direccion = models.CharField(max_length=200)
+    comuna = models.CharField(max_length=100)
+    tipo_inmueble = models.CharField(max_length=50)
+    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
