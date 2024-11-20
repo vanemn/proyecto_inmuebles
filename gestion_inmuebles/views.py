8f5aa2e69de13b15464d904baf9ea6f7f545b48a
Vane <vane.moralesn@gmail.com>
2024-11-20 19:21:49 -0300

    se ajustan las plantillas se extiende layout a otros templates usando {% extends 'base.html' %}
    {% block title %}Inicio{% endblock %}
    
    {% block content %}
    
    <p>Contenido </p>
    
    {% endblock %}
     se configuran las urls para las vistas

--- gestion_inmuebles/views.py
+++ gestion_inmuebles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-   
     return render(request, 'home.html', {})
 
+# Función para mostrar la página "Acerca de"
+def about(request):
+	return render(request, 'about.html', {})	
