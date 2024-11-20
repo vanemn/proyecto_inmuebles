ee7c89349074ab9e3706959d76c2eecb3d7a48f2
Vane <vane.moralesn@gmail.com>
2024-11-19 23:34:50 -0300

    Se instala libreria con comando pip install pillow , se crea media para imagenes, se ajusta settings y urls. se modifica el modelo considerando que tendrá filtros la selección de inmuebles y se agregan al admin, se realizan las migraciones respectivas

--- gestion_inmuebles/admin.py
+++ gestion_inmuebles/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
+from .models import Region, Comuna, Inmueble
 
-from gestion_inmuebles.models import Inmueble
 
-# Register your models here.
-admin.site.register(Inmueble)
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(Comuna)
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+
+@admin.register(Inmueble)
+class InmuebleAdmin(admin.ModelAdmin):
+    list_display = (
+        "comuna",
+        "estado",
+        "precio_min",
+        "precio_max",
+        "tipo_inmueble",
+        "fecha_inicio",
+        "fecha_fin",
+        "nombre_autor",
+        "titulo_autor",
+    )
+    search_fields = ("comuna__nombre", "estado", "nombre_autor")
+    list_filter = (
+        "estado",
+        "tipo_inmueble",
+        "dormitorios",
+        "baños",
+        "precio_min",
+        "precio_max",
+    )
--- gestion_inmuebles/models.py
+++ gestion_inmuebles/models.py
@@ -1,19 +1,54 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Inmueble(models.Model):
 
+    ESTADO_CHOICES = [
+        ("reservada", "Reservada"),
+        ("en_alquiler", "En Alquiler"),
+    ]
+
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, default=1)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE, default=1)
     nombre = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    dormitorios = models.PositiveIntegerField(default=1)
+    baños = models.PositiveIntegerField(default=1)
+    estado = models.CharField(
+        max_length=20, choices=ESTADO_CHOICES, default="en_alquiler"
+    )
+    precio_min = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    precio_max = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    tipo_inmueble = models.CharField(max_length=50)
+    fecha_inicio = models.DateField(default=timezone.now)
+    fecha_fin = models.DateField(default=timezone.now)
+    imagen = models.ImageField(upload_to="imagenes_inmuebles/", null=True, blank=True)
+    descripcion = models.TextField(max_length=250, default="", blank=True, null=True)
     m2_construidos = models.FloatField()
     m2_totales = models.FloatField()
-    estacionamientos = models.IntegerField()
-    habitaciones = models.IntegerField()
-    banos = models.IntegerField()
-    direccion = models.CharField(max_length=200)
-    comuna = models.CharField(max_length=100)
-    tipo_inmueble = models.CharField(max_length=50)
-    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
+    nombre_autor = models.CharField(max_length=100, default="Desconocido")
+    titulo_autor = models.CharField(max_length=100, default="Desconocido")
+    imagen_autor = models.ImageField(
+        upload_to="imagenes_autores/", null=True, blank=True
+    )
+    estacionamientos = models.IntegerField(null=True, blank=True)
+    precio_mensual = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
 
     def __str__(self):
-        return self.nombre
+        return f"{self.comuna} - {self.estado}"
