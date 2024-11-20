fb2efb0be28470f715efa29c6ce3fd0766680874
Vane <vane.moralesn@gmail.com>
2024-11-20 20:39:54 -0300

    creación de plantillas login logout, registro de path en urls.py e incorporacion en views.py

--- gestion_inmuebles/views.py
+++ gestion_inmuebles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout # Para gestionar la autenticación de usuarios
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,11 @@ def home(request):
 # Función para mostrar la página "Acerca de"
 def about(request):
 	return render(request, 'about.html', {})	
+
+# Función para iniciar sesión del usuario
+def login_user(request):
+      return render(request, 'login.html', {})
+
+# Función para cerrar sesión del usuario
+def logout_user(request):
+    pass
