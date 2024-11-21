19959223972a46953af368bf30537e3c4c2917a5
Vane <vane.moralesn@gmail.com>
2024-11-20 22:11:11 -0300

    login, logout  ingreso de usurios, alertas configuracion de las plantillas

--- gestion_inmuebles/views.py
+++ gestion_inmuebles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout # Para gestionar la autenticación de usuarios
 from django.contrib import messages # Para mostrar mensajes de notificación al usuario
 
@@ -12,8 +12,22 @@ def about(request):
 
 # Función para iniciar sesión del usuario
 def login_user(request):
-      return render(request, 'login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "¡Ya ingresaste!")
+            return redirect('home')
+        else:
+            messages.error(request, "Hay un error, intenta una vez más...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
 
 # Función para cerrar sesión del usuario
 def logout_user(request):
-    pass
+	logout(request)
+	messages.success(request, ("ya hiciste logout, gracias por esta sesión..."))
+	return redirect('home')
